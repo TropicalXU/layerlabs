711a617cb1cb78e1491544a82f996d81fa762149
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-10-03 00:36:34 -0400

    updates to projects

--- routes/projects.js
+++ routes/projects.js
@@ -4,26 +4,25 @@ const router = express.Router();
 
 router.get('/images/IMG_7975.png', (req, res) => {
     res.render('home')
-})
+});
 
 //rendering the work page
 router.get('/', async (req, res) => {
     res.render('main/projects');
-})
+});
 
 //rendering ViewpointIreland/project page
 router.get('/viewpointIreland', async(req, res) => {
     res.render('projects/viewpointIreland')
-})
+});
 
 //rendering ShirleysArtStudio/project page
 router.get('/shirleys-art-studio', (req, res) => {
     res.render('projects/shirleys-art-studio')
-})
+});
 
-//rendering 
-router.get('/defineDesign', async(req, res) => {
-    res.render('projects/defineDesign')
-})
+router.get('/vacay', (req, res) => {
+    res.render('projects/vacay')
+});
 
 module.exports = router;
